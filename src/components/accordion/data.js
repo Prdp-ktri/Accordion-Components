dcd0cfd4e028e1eb120e9317db542ad51d83745c
Pradeep Khatri <pradeepkhatri44@live.com>
2025-01-31 19:12:09 +0530

    Adding the Components folder
    Adding the Components folder

---
+++ src/components/accordion/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: "1",
+    question: "What are Accordion Components?",
+    answer:
+      "Accordion Components are user inerface elements used for organizing and presenting",
+  },
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer:
+      "They are commonly employed in various contexts, including FAQs, product description",
+  },
+  {
+    id: "3",
+    question: "Accordion as musical instrument",
+    answer:
+      "The accordion is a musical instrument with a keyboard and bellows. It produces sound.",
+  },
+  {
+    id: "4",
+    question:
+      "Can I create an accordion components with a different framework?",
+    answer:
+      "Yes of course, it is very possible to create an accordion component with another framework",
+  },
+];
+
+export default data;
